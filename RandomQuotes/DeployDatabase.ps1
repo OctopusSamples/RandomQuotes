be031449fb14b6e0db83142a9af61b6cb90bb502
Rob Pearson <rob.pearson@me.com>
2018-07-10 14:24:52 +1000

    Updated database deployment script.

--- RandomQuotes/DeployDatabase.ps1
+++ RandomQuotes/DeployDatabase.ps1
@@ -2,4 +2,29 @@
 # DeployDatabase.ps1
 #
 
-Write-Host "Hello from a package"
+Param (
+	[Parameter(Mandatory=$True)]
+	[string]$SqlFilePath,
+	[Parameter(Mandatory=$True)]
+	[string]$SqlServerName,
+	[Parameter(Mandatory=$True)]
+	[string]$SqlServerUserName,
+	[Parameter(Mandatory=$True)]
+	[string]$SqlServerUserPassword
+)
+ 
+Write-Host "Starting Database Deployment to $SqlServerName"
+
+#$results = @() 
+
+#-InputFile "C:\ScriptFolder\TestSqlCmd.sql" `
+#Invoke-Sqlcmd -Query "select SERVERPROPERTY('ServerName') as Server, count(*) as 'DB Count' from sys.databases" `
+#	-ServerInstance $instanceName `
+#    -U $db.user.name -P $db.user.password
+
+#$results += Invoke-Sqlcmd `
+ 
+# Print Results
+#$results | Format-Table -autosize
+
+Write-Host "Finished Database Deployment..."
