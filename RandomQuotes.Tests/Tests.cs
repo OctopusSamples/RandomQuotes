9e7134c320ff02ce857d631584846d46203ebd4f
Matthew Casperson <matthewcasperson@gmail.com>
2019-10-28 15:31:18 +1000

    Added tests project

---
+++ RandomQuotes.Tests/Tests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RandomQuotes.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
